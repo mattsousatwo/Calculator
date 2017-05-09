c23ccfb4c77019962d7e0a011da572ee815015c6
Matt Sousa <mattsousatwo@gmai.com>
2017-05-09 01:01:49 -0400

    Moved Buttons: - I realized that I put the buttons inside the LaunchScreen.Storyboard file, so I deleted the buttons from the LaunchScreen file and moved them to the Main.storyboard.   - I edited the buttons in the Main.storyboard to be the right text and changed the color of the buttons and text.
    Added Actions to Veiw Controller:
    I tried to add all the buttons to the view controller with one IBAction, although I think I only was able to attach the 6 button. This is because the 6 button is the only button that works on the UI.
    I also added two lines of code to change the labels text to a String of the buttons being pressed, which has been unwrapped to a Double value.

--- CalculatorProject/ViewController.swift
+++ CalculatorProject/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberOnLabel: Double = 0
+    var numberOnScreen: Double = 0
+    
+    
+    @IBOutlet weak var printOutLabel: UILabel!
+    
+    
+    
+    /// I think only the 6 button was attached to this IBAction
+    @IBAction func numbers(_ sender: UIButton) {
+        // label.text will be updated to the labels text and the buttons tag value minus one
+       printOutLabel.text = String(sender.tag-1)
+        
+        numberOnScreen = Double(printOutLabel.text!)!
+        // fatal error (when I press 6, when i press any other button nothing happens, text does not change)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
