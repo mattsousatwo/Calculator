20c6ebc68f20aba66b0176be40f2050217397ef7
Matt Sousa <mattsousatwo@gmai.com>
2017-05-11 01:48:30 -0400

    Adding Multiple Numbers Into the Label: - I was able to use the same code ,printOutLabel.text = String(sender.tag-1) + printOutLabel.text!, to add multiple numbers to the screen as they are being pressed. I did this by removing the string "Numbers go Here" from the labels default text. 	- this revealed another bug, that when I add an operator symbol to the label, numbersOnScreen expects printOutLabel.text to be equal to a Double value
    operators:
    - added the clear button to the operators func

--- CalculatorProject/ViewController.swift
+++ CalculatorProject/ViewController.swift
@@ -19,27 +19,30 @@ class ViewController: UIViewController {
     
     @IBAction func numbers(_ sender: UIButton) {
         
-        printOutLabel.text = String(sender.tag-1)
+        printOutLabel.text = String(sender.tag-1) + printOutLabel.text!
         numbersOnScreen = Double(printOutLabel.text!)!
         
-        //  Fatal Error : printOutLabel.text = printOutLabel.text! + String(sender.tag-1)
+        //  Fatal Error : printOutLabel.text = printOutLabel.text! + String(sender.tag-1)  May 10th
             // "fatal error: unexpectedly found nil while unwrapping an Optional value"
-        
+        //  May 11th
+            // Fixed this problem by changing the text on the label to "" by default instead of "Numbers go here"
+                // The numbersOnScreen expects a Double to be on Label.text This is why the app crashed then, and is why it is crashing now. Whenever I press an operator symbol the application is fine but if I try to press a number after that, it will crash because + is not a number
         
         // when I added "Label.text! +" numbersOnScreen returned a fatal error
         // everything worked fine until I added "Label.text! +", although I could only display the number being pressed on the screen with no additional numbers
     }
     
     
-   
+    // Want to add a method to change the apperance of the buttons when pressed so I dont have to print out the characters on the label
     @IBAction func operators(_ sender: UIButton) {
         printOutLabel.text = printOutLabel.text! + String(sender.tag-1)
         
-        if sender.tag == 15 { // division
+        if sender.tag == 15 { // division   //  ÷
             printOutLabel.text = "/"
         }
         else if sender.tag == 16 { // multiplication
             printOutLabel.text = "X"
+            
         }
         else if sender.tag == 17 { // minus
             printOutLabel.text = "-"
@@ -49,6 +52,9 @@ class ViewController: UIViewController {
         }
         else if sender.tag == 19 { // equals
             printOutLabel.text = "="
+        }
+        else if sender.tag == 11 { // clear
+            printOutLabel.text = ""
         }
     }
     
